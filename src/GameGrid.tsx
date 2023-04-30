f9d1a7d13f8f19bda135826bd2e1a0fbbfff6539
cfarmer-fearless <cfarmer@fearless.tech>
2023-04-30 13:05:59 -0400

    detect wall hit

--- src/GameGrid.tsx
+++ src/GameGrid.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useReducer, useState } from "react";
+import { useCallback, useEffect, useReducer, useRef, useState } from "react";
 import GridBox from "./GridBox";
 
 import classes from "./styles/GameGrid.module.css";
@@ -24,6 +24,28 @@ export enum Direction {
 type SnakeAction = { type: "tick"; payload: Direction } | { type: "eat" };
 
 const BOARD_SIZE = 15;
+
+// Eventually, this can take a difficulty to change the size of the grid.
+const createInitialGrid = () => {
+  const grid = Array<GridNode[]>();
+  let count = 0;
+
+  for (let i = 0; i < BOARD_SIZE; i++) {
+    const row = Array<GridNode>();
+
+    for (let j = 0; j < BOARD_SIZE; j++) {
+      row.push({
+        id: count,
+        hasSnake: false,
+        hasFood: false,
+      });
+      count++;
+    }
+
+    grid.push(row);
+  }
+  return grid;
+};
 
 const snakeReducer = (
   state: SnakePiece[],
@@ -75,31 +97,9 @@ const moveSnakeOneUnit = (
 };
 
 const GameGrid: React.FC = () => {
-  const [gridState, setGridState] = useState<GridNode[][]>([[]]);
+  const gridRef = useRef<GridNode[][]>(createInitialGrid());
   const [snake, dispatch] = useReducer(snakeReducer, [{ row: 1, col: 1 }]);
   const arrowPress = useArrowKeyPress();
-
-  useEffect(() => {
-    // Init grid.
-    const grid = Array<GridNode[]>();
-    let count = 0;
-
-    for (let i = 0; i < BOARD_SIZE; i++) {
-      const row = Array<GridNode>();
-
-      for (let j = 0; j < BOARD_SIZE; j++) {
-        row.push({
-          id: count,
-          hasSnake: false,
-          hasFood: false,
-        });
-        count++;
-      }
-
-      grid.push(row);
-    }
-    setGridState(grid);
-  }, []);
 
   useEffect(() => {
     // Init game interval.
@@ -110,6 +110,13 @@ const GameGrid: React.FC = () => {
 
     return () => clearInterval(gameInterval);
   }, [arrowPress]);
+
+  // Detect game over.
+  useEffect(() => {
+    if (didHitWall(snake, gridRef.current)) {
+      console.log("GAME OVER");
+    }
+  }, [snake]);
 
   const nodeHasSnakePiece = (row: number, col: number) => {
     let hasPiece = false;
@@ -124,12 +131,21 @@ const GameGrid: React.FC = () => {
     return hasPiece;
   };
 
+  const didHitWall = (snakeState: SnakePiece[], grid: GridNode[][]) => {
+    return (
+      snakeState[0].row >= grid.length ||
+      snakeState[0].col >= grid[0].length ||
+      snakeState[0].row < 0 ||
+      snakeState[0].col < 0
+    );
+  };
+
   return (
     <div
       className={classes.gameGrid}
       style={{ gridTemplateColumns: `repeat(${BOARD_SIZE}, auto)` }}
     >
-      {gridState.map((gridRow, row) =>
+      {gridRef.current.map((gridRow, row) =>
         gridRow.map((gridItem, col) => (
           <GridBox
             key={gridItem.id}
